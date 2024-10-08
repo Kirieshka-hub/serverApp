d51afed410d8e08feecbb5d3e7bbd3f4214e021c
Mikhail <mihailohrimenko300@gmail.com>
2024-10-08 20:22:55 +1000

    Added file ".gitignore", made user interface

---
+++ tst.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5 import QtWidgets
+from tic_tac_toe_ui import Ui_MainWindow
+
+
+class MyApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyApp, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
